04d3fa3411d5c5da21bfce27dd6f8ccbfaa474f8
Gianmarko Pereda Zarate <gianmarko@Gianmarkos-MacBook-Air.local>
2024-12-02 15:10:31 -0500

    Programa inicial: modelos creados para ser definidos

---
+++ kronoslog/src/main/java/com/orbistech/kronoslog/KronoslogApplication.java
@@ -0,0 +1,13 @@
+package com.orbistech.kronoslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KronoslogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KronoslogApplication.class, args);
+    }
+
+}
---
+++ kronoslog/src/main/java/model/Rol.java
@@ -0,0 +1,17 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombreRol;
+    private String descripcionRol;
+}
